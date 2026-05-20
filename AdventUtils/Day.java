b014a5e537613797d3185f044207a6edd0aac370
Mr-Azam-AHS <eazam@andrews.esc18.net>
2026-05-20 13:23:53 -0500

    Day1 complete

--- AdventUtils/Day.java
+++ AdventUtils/Day.java
@@ -2,7 +2,10 @@ package AdventUtils;
 
 public interface Day {
     default void runAll() {
+        System.out.println("Running part 1...\n");
         part1();
+
+        System.out.println("Running part 2...\n");
         part2();
     }
 
